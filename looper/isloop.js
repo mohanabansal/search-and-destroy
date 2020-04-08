d340f49b2dfa0e3e72b7244824cd658bf245b91a
Mohana Bansal <mohanabansal10@gmail.com>
2020-04-08 16:39:36 -0400

    isLoop completed

--- looper/isloop.js
+++ looper/isloop.js
@@ -2,7 +2,22 @@
 
 //Complete this algo
 const isLoop = (linkedlist) => {
-
+  let slow = linkedlist.head;
+  let fast = linkedlist.head;
+  while(true) {
+    slow = slow.next;
+    if(fast.next !== null) {
+      fast = fast.next.next;
+    }else {
+      return false;
+    }
+    if(slow === null || fast === null) {
+      return false
+    }
+    if(slow == fast) {
+      return true
+    }
+  }
 };
 
 
