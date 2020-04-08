2b66c3036c5f60628f3310cf4637c64d6ac0486b
Mohana Bansal <mohanabansal10@gmail.com>
2020-04-08 15:38:51 -0400

    WIP - binary search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,25 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	let flag = false;
+	let mid = Math.floor(array.length/2);
+	console.log("first mid", array[mid])
+	let low = 0;
+	let high = array.length-1;
+	console.log("low", low, "high", high)
+	while(array[mid] !== target && low <= high) {
+		console.log("inside while")
+		if( array[mid] < target) {
+			high = mid-1;
+		}else {
+			low = mid+1;
+		}
+		console.log("final mid", array[mid])
+		mid = Math.floor((low+high)/2);
+	}
+	if(array[mid] === target) return true
+	else return false
+	// return false
 };
 
 /*
